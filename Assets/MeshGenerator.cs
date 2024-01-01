ae4756063badb82af4efb5923d8dca24713b914c
Resit Kaya <resit.kaya@protonmail.com>
2024-01-01 12:45:32 +0300

    mesh generator

--- Assets/MeshGenerator.cs
+++ Assets/MeshGenerator.cs
@@ -2,17 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class MeshGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Mesh mesh;
+
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    private void Start()
     {
-        
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateMesh();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateShape()
     {
-        
+        vertices = new Vector3[]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(0, 0, 1),
+            new Vector3(1, 0, 0)
+        };
+
+        triangles = new int[]
+        {
+            0, 1, 2
+        };
+    }
+
+    private void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+            return;
+
+        Gizmos.color = Color.red;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
     }
 }
